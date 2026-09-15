980c152f2291fb54446aa41a1fe0e252e9997f33
shawncalvinsnelling <Axezentai@gmail.com>
2026-09-15 01:57:32 -0500

    Build finite signed-class module

--- formal/lakefile.lean
+++ formal/lakefile.lean
@@ -14,5 +14,6 @@ lean_lib OPAC016Formal where
     `OPAC016RootSpanned,
     `OPAC016BalancedProjection,
     `OPAC016BalancedPolytope,
-    `OPAC016SignedComponents
+    `OPAC016SignedComponents,
+    `OPAC016SignedClass
   ]
